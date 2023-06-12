ed220e47996d743de6a0889bbe337cad27ed951f
Great <great.goodwin.92@gmail.com>
2023-06-12 16:02:37 +0300

    дз 20.2

--- catalog/views.py
+++ catalog/views.py
@@ -1,10 +1,17 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from catalog.models import Product
 
 
 def home(request):
-    return render(request, 'catalog/home.html')
+    data = Product.objects.all()
+    return render(request, 'catalog/home.html', {'data': data})
 
 
 def contact(request):
     return render(request, 'catalog/contacts.html')
+
+
+def product(request, product_id):
+    data = Product.objects.get(id=product_id)
+    return render(request, 'catalog/product.html', {'data': data})
