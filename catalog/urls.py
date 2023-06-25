799702e786c725ee8af88910852877a5e1fe6b9b
Great <great.goodwin.92@gmail.com>
2023-06-25 23:12:22 +0300

    21.1 готово

--- catalog/urls.py
+++ catalog/urls.py
@@ -8,5 +8,10 @@ urlpatterns = [
     path('', views.ProductsListView.as_view(), name='product_list'),
     path('contact/', views.ContactView.as_view(), name='contact'),
     path('product/<int:pk>/', views.ProductDetailView.as_view(), name='product_page'),
+    path('blog/', views.BlogListView.as_view(), name='blog_list'),
+    path('blog/create/', views.BlogCreateView.as_view(), name='blog_create'),
+    path('blog/<slug:slug>/', views.BlogDetailView.as_view(), name='blog_detail'),
+    path('blog/<slug:slug>/update/', views.BlogUpdateView.as_view(), name='blog_update'),
+    path('blog/<slug:slug>/delete/', views.BlogDeleteView.as_view(), name='blog_delete')
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
 
--- catalog/views.py
+++ catalog/views.py
@@ -1,7 +1,9 @@
 from django.shortcuts import render
 from django.http import HttpResponse
-from catalog.models import Product
+from slugify import slugify
+from catalog.models import Product, Blog
 from django.views import generic
+from django.urls import reverse_lazy, reverse
 
 
 class ProductsListView(generic.ListView):
@@ -15,3 +17,49 @@ class ContactView(generic.TemplateView):
 class ProductDetailView(generic.DetailView):
     model = Product
 
+
+class BlogListView(generic.ListView):
+    model = Blog
+
+    def get_queryset(self):
+        queryset = super().get_queryset()
+        queryset = queryset.filter(is_published=True)
+        return queryset
+
+
+class BlogDetailView(generic.DetailView):
+    model = Blog
+    slug_url_kwarg = 'slug'
+
+    def get_context_data(self, *args, **kwargs):
+        context = super().get_context_data(**kwargs)
+        self.object.count_views += 1
+        self.object.save()
+        return context
+
+
+class BlogCreateView(generic.CreateView):
+    model = Blog
+    fields = ('title', 'content', 'image')
+    success_url = reverse_lazy('blog_list')
+
+    def form_valid(self, form):
+        response = super().form_valid(form)
+        return response
+
+
+class BlogUpdateView(generic.UpdateView):
+    model = Blog
+    fields = ('title', 'content', 'image')
+
+    def form_valid(self, form):
+        response = super().form_valid(form)
+        return response
+
+    def get_success_url(self):
+        return reverse('blog_detail', kwargs={'slug': self.object.slug})
+
+
+class BlogDeleteView(generic.DeleteView):
+    model = Blog
+    success_url = reverse_lazy('blog_list')
